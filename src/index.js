9e4a9f8a0a28cd6a535cb9bd6c4585b07fe7f1c5
Ricou107 <76947043+Ricou107@users.noreply.github.com>
2023-05-12 17:25:26 +0200

    delete web vitals

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./styles/main.css";
 import { Provider } from "react-redux";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import store from "./app/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -15,4 +14,3 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
